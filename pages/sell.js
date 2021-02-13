5f20d5e0f616a3c672775bb3a4397c68f6a6408f
Martins Siksnis <martinssiksnis@Martinss-MacBook-Pro.local>
2021-02-13 23:45:50 +0100

    Created UpdateProduct component in which now I can update a product.

--- pages/sell.js
+++ pages/sell.js
@@ -1,6 +1,4 @@
-// import { set } from "nprogress";
 import CreateProduct from "../components/CreateProduct";
-// import Page from "../components/Page";
 
 function SellPage() {
   return <CreateProduct />;
